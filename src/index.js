7fffba0a0126bfb5ac348b98845486bc39a37569
Rabbid <busnka777@gmail.com>
2022-05-29 19:26:42 +0300

    add restart, update drop-functions

--- src/index.js
+++ src/index.js
@@ -3,244 +3,246 @@ const dt = 0.05;
 let isWin = false;
 let isEnd = false;
 
-
-
-// document.querySelector(`#new-game-button`).addEventListener(`click`,startGame);
-
-
-function startGame(startAngle,startSpeed){
-    const room = new Room();
-    const ball = new Ball(startSpeed,startAngle);
-    const basket = new Basket();
-    //this.classList.toggle(`hide`);
-    let gameTimer = setInterval(()=>{
-        if(room.isBarrier(ball,basket)){    
-            let correctCoordinates = room.getСorrectСoordinates(ball,basket);
-            ball.setPosition(correctCoordinates.x,correctCoordinates.y);
-            ball.changeDirection(basket);
-        }
-        ball.move();
-        if (ball.isStatic) {
-            clearInterval(gameTimer);
-            ball.setPosition(ball.box.center.x, ball.box.height/2 +1);
-            if (!isEnd) endGame();
-        }
-        if (basket.isCaught(ball) && !isWin){
-            isWin = true;
-            if (!isEnd) endGame();
-            ball.setPosition(basket.box.center.x,ball.box.center.y);
-            ball.changeDirection(basket);
-        }
-    },dt*1000);
-}
-
-
-
-function Coordinates(x,y) {
-    this.x = x;
-    this.y = y;
-}
-
-function Box(element){
-    this.width = parseInt(window.getComputedStyle(element).width,10);
-    this.height = parseInt(window.getComputedStyle(element).height,10);
-    this.x0 = parseInt(window.getComputedStyle(element).left,10);
-    this.y0 = parseInt(window.getComputedStyle(element).bottom,10);
-    this.x1 = this.x0 + this.width;
-    this.y1 = this.y0 + this.height;
-    let midX = this.x0 + Math.floor(this.width/2);
-    let midY = this.y0 + Math.floor(this.height/2);
-    this.center = new Coordinates(midX, midY);
-
-    this.updatePosition = function (x,y){
-        let halfWidth = Math.floor(this.width/2);
-        let halfHeight =  Math.floor(this.height/2);
-        this.center.x = x;
-        this.center.y = y;
-        this.x0 = x - halfWidth;
-        this.y0 = y - halfHeight;
-        this.x1 = x + halfWidth;
-        this.y1 = y + halfHeight;
+function startGame(startAngle, startSpeed) {
+  const room = new Room();
+  const ball = new Ball(startSpeed, startAngle);
+  const basket = new Basket();
+  let gameTimer = setInterval(() => {
+    if (room.isBarrier(ball, basket)) {
+      let correctCoordinates = room.getСorrectСoordinates(ball, basket);
+      ball.setPosition(correctCoordinates.x, correctCoordinates.y);
+      ball.changeDirection(basket);
     }
-
-}
-
-function Ball (speed, throwAangle) {
-    this.element= document.querySelector(`#ball`);
-    this.box = new Box(this.element);
-    this.direction = {
-        isToRight:true,
-        isUp:true,
-    };
-    this.isStatic = false;
-    let v0 = speed;
-    let angle = throwAangle;
-    let t = 1;
-    let vx = 0;
-    let vy =0;
-    let v= 0;
-
-    this.setPosition = function(x,y){
-        this.box.updatePosition(x,y);
-        shiftObject(this);
-    };
-
-    this.move = function(){
-        calcValues(this);
-        shiftObject(this);
-        if (isWin) isWin = false;
-    };
-
-    this.changeDirection = function(basket){
-        angle = getNewAngle(vx,vy,this,basket);
-        v0 = v0 * 0.8;
-        t=1;
-        this.isStatic = (Math.floor(v0) === 0 && this.box.y0 <=1);
+    ball.move();
+    if (ball.isStatic) {
+      clearInterval(gameTimer);
+      ball.setPosition(ball.box.center.x, ball.box.height / 2 + 1);
+      if (!isEnd) {
+        endGame();
+      }
+      startNewGame();
     }
-
-    function calcValues(ball){
-        t += dt;
-        let x = ball.box.center.x + getX(v0, t, angle);
-        let y = ball.box.center.y + getY(v0, t, angle);
-        updateDirection(x,y,ball);
-        vx = getSpeedX(v0,angle);
-        vy = getSpeedY(v0,angle,t);
-        v = getSpeed(vx,vy);
-        ball.box.updatePosition(x,y);
-    };
-
-    function getX(v, t, angle){
-        return v * t * Math.cos(angle);
-    };
-    
-    function getY(v, t, angle){
-        return v * t * Math.sin(angle) - (g * Math.pow(t,2))/2;
-    };
-
-    function updateDirection(x,y,ball){
-        ball.direction.isUp = y > ball.box.center.y;
-        ball.direction.isToRight = x > ball.box.center.x;
+    if (basket.isCaught(ball) && !isWin) {
+      isWin = true;
+      if (!isEnd) endGame();
+      ball.setPosition(basket.box.center.x, ball.box.center.y);
+      ball.changeDirection(basket);
     }
-    
-    function getSpeedX(v0,angle){
-        return v0 * Math.cos(angle);
-    };
-    
-    function getSpeedY(v0,angle,t){
-        return v0*Math.sin(angle) - g * t;
-    };
-    
-    function getSpeed(vx,vy){
-        return Math.pow((Math.pow(vx,2)+Math.pow(vy,2)),1/2);
-    };
+  }, dt * 1000);
+}
 
-    //За это мне стыдно! Может однажды переосмыслю эти углы.
-    function getNewAngle(vx,vy,ball,basket){
-        vy = getCorrectVy(vy,ball);
-        let b = Math.atan(vy/vx);
+function Coordinates(x, y) {
+  this.x = x;
+  this.y = y;
+}
 
-        if (isWin && isEnd){
-            return -Math.PI/2;
-        } else if (ball.box.y0 <=1){
-            if (b < 3*Math.PI/2 && b >0) return Math.PI-b;
-            else if (b < 0) return Math.abs(b);
-            else return 2*Math.PI -b;
-        } else if (ball.box.y1 >=windowHeight-1){
-            if (b <Math.PI/2 && b>0) return Math.PI + b;
-            else return b - Math.PI;
-        } else if (ball.box.x0 <= 1){
-            return Math.abs(b);
-        } else if (ball.box.x1 >= basket.box.x0 -1 ){
-            if (b < 0) return Math.PI - Math.abs(b);
-            else if (ball.isUp) return Math.PI+b;
-            else return b - Math.PI;
-        }
-    };
+function Box(element) {
+  this.width = parseInt(window.getComputedStyle(element).width, 10);
+  this.height = parseInt(window.getComputedStyle(element).height, 10);
+  this.x0 = parseInt(window.getComputedStyle(element).left, 10);
+  this.y0 = parseInt(window.getComputedStyle(element).bottom, 10);
+  this.x1 = this.x0 + this.width;
+  this.y1 = this.y0 + this.height;
+  let midX = this.x0 + Math.floor(this.width / 2);
+  let midY = this.y0 + Math.floor(this.height / 2);
+  this.center = new Coordinates(midX, midY);
 
-    function getCorrectVy(vy,ball){
-        if ((ball.isUp && vy<0) || (!ball.isUp && vy>0)) return -vy;
-        return vy;
+  this.updatePosition = function (x, y) {
+    let halfWidth = Math.floor(this.width / 2);
+    let halfHeight = Math.floor(this.height / 2);
+    this.center.x = x;
+    this.center.y = y;
+    this.x0 = x - halfWidth;
+    this.y0 = y - halfHeight;
+    this.x1 = x + halfWidth;
+    this.y1 = y + halfHeight;
+  };
+}
+
+function Ball(speed, throwAangle) {
+  this.element = document.querySelector(`#ball`);
+  this.box = new Box(this.element);
+  this.direction = {
+    isToRight: true,
+    isUp: true,
+  };
+  this.isStatic = false;
+  let v0 = speed;
+  let angle = throwAangle;
+  let t = 1;
+  let vx = 0;
+  let vy = 0;
+  let v = 0;
+
+  this.setPosition = function (x, y) {
+    this.box.updatePosition(x, y);
+    shiftObject(this);
+  };
+
+  this.move = function () {
+    calcValues(this);
+    shiftObject(this);
+    if (isWin) isWin = false;
+  };
+
+  this.changeDirection = function (basket) {
+    angle = getNewAngle(vx, vy, this, basket);
+    v0 = v0 * 0.8;
+    t = 1;
+    this.isStatic = Math.floor(v0) === 0 && this.box.y0 <= 1;
+  };
+
+  function calcValues(ball) {
+    t += dt;
+    let x = ball.box.center.x + getX(v0, t, angle);
+    let y = ball.box.center.y + getY(v0, t, angle);
+    updateDirection(x, y, ball);
+    vx = getSpeedX(v0, angle);
+    vy = getSpeedY(v0, angle, t);
+    v = getSpeed(vx, vy);
+    ball.box.updatePosition(x, y);
+  }
+
+  function getX(v, t, angle) {
+    return v * t * Math.cos(angle);
+  }
+
+  function getY(v, t, angle) {
+    return v * t * Math.sin(angle) - (g * Math.pow(t, 2)) / 2;
+  }
+
+  function updateDirection(x, y, ball) {
+    ball.direction.isUp = y > ball.box.center.y;
+    ball.direction.isToRight = x > ball.box.center.x;
+  }
+
+  function getSpeedX(v0, angle) {
+    return v0 * Math.cos(angle);
+  }
+
+  function getSpeedY(v0, angle, t) {
+    return v0 * Math.sin(angle) - g * t;
+  }
+
+  function getSpeed(vx, vy) {
+    return Math.pow(Math.pow(vx, 2) + Math.pow(vy, 2), 1 / 2);
+  }
+
+  //За это мне стыдно! Может однажды переосмыслю эти углы.
+  function getNewAngle(vx, vy, ball, basket) {
+    vy = getCorrectVy(vy, ball);
+    let b = Math.atan(vy / vx);
+
+    if (isWin && isEnd) {
+      return -Math.PI / 2;
+    } else if (ball.box.y0 <= 1) {
+      if (b < (3 * Math.PI) / 2 && b > 0) return Math.PI - b;
+      else if (b < 0) return Math.abs(b);
+      else return 2 * Math.PI - b;
+    } else if (ball.box.y1 >= windowHeight - 1) {
+      if (b < Math.PI / 2 && b > 0) return Math.PI + b;
+      else return b - Math.PI;
+    } else if (ball.box.x0 <= 1) {
+      return Math.abs(b);
+    } else if (ball.box.x1 >= basket.box.x0 - 1) {
+      if (b < 0) return Math.PI - Math.abs(b);
+      else if (ball.isUp) return Math.PI + b;
+      else return b - Math.PI;
     }
+  }
+
+  function getCorrectVy(vy, ball) {
+    if ((ball.isUp && vy < 0) || (!ball.isUp && vy > 0)) return -vy;
+    return vy;
+  }
 }
 
-function Basket () {
-    this.element = document.querySelector(`#basket`);
-    this.box = new Box(this.element);
+function Basket() {
+  this.element = document.querySelector(`#basket`);
+  this.box = new Box(this.element);
 
-    this.isBasketEdge = function(ball){
-        return (ball.box.x1 >= this.box.x0) &&
-               (ball.box.x0 <= this.box.x0) && 
-               (ball.box.y0 <= this.box.center.y) &&
-               (ball.box.y1 >= this.box.center.y);
-    };
+  this.isBasketEdge = function (ball) {
+    return (
+      ball.box.x1 >= this.box.x0 &&
+      ball.box.x0 <= this.box.x0 &&
+      ball.box.y0 <= this.box.center.y &&
+      ball.box.y1 >= this.box.center.y
+    );
+  };
 
-    this.isCaught = function(ball){
-        return (ball.box.center.x > this.box.x0) && 
-               (ball.box.x1 <= this.box.x1)&&
-               (ball.box.center.y >= this.box.center.y) &&
-               (ball.box.y0 <= this.box.y1);
-    };
-
+  this.isCaught = function (ball) {
+    return (
+      ball.box.center.x > this.box.x0 &&
+      ball.box.x1 <= this.box.x1 &&
+      ball.box.center.y >= this.box.center.y &&
+      ball.box.y0 <= this.box.y1
+    );
+  };
 }
 
-function Room (){
-    this.element = document.querySelector(`#room`);
-    this.box = new Box(this.element);
-    let isFloor = false;
-    let isCeiling = false;
-    let isLeftWall = false;
-    let isRightWall = false;
-    let isBasketEdge = false;
+function Room() {
+  this.element = document.querySelector(`#room`);
+  this.box = new Box(this.element);
+  let isFloor = false;
+  let isCeiling = false;
+  let isLeftWall = false;
+  let isRightWall = false;
+  let isBasketEdge = false;
 
-    this.isBarrier = function(ball,basket){
-        isBasketEdge = basket.isBasketEdge(ball);
-        isFloor = ball.box.y0 <= this.box.y0;
-        isCeiling = ball.box.y1 >= this.box.y1;
-        isLeftWall = ball.box.x0 <= this.box.x0;
-        isRightWall = ball.box.x1 >=this.box.x1;
+  this.isBarrier = function (ball, basket) {
+    isBasketEdge = basket.isBasketEdge(ball);
+    isFloor = ball.box.y0 <= this.box.y0;
+    isCeiling = ball.box.y1 >= this.box.y1;
+    isLeftWall = ball.box.x0 <= this.box.x0;
+    isRightWall = ball.box.x1 >= this.box.x1;
 
-        return (isBasketEdge || isFloor || isCeiling || isLeftWall || isRightWall);
-    };
+    return isBasketEdge || isFloor || isCeiling || isLeftWall || isRightWall;
+  };
 
-    this.getСorrectСoordinates = function(ball,basket){
-        let newCoordinates = new Coordinates(ball.box.center.x,ball.box.center.y);
-        let halfWidth = Math.floor(ball.box.width/2);
-        let halfHeight = Math.floor(ball.box.height/2);
-        if (isBasketEdge) {
-            isBasketEdge = false;
-            newCoordinates.x = basket.box.x0-halfWidth - 1;
-        }
-        if (isFloor) {
-            isFloor = false;
-            newCoordinates.y = this.box.y0 + halfHeight + 1;
-        }
-        if (isCeiling) {
-            isCeiling = false;
-            newCoordinates.y = this.box.y1- halfHeight - 1;
-        }
-        if (isLeftWall) {
-            isLeftWall =false; 
-            newCoordinates.x = this.box.x0 + halfWidth +1;
-        }
-        if (isRightWall) {
-            isRightWall = false;
-            newCoordinates.x = this.box.x1-halfWidth -1;
-        }
-        return newCoordinates;
+  this.getСorrectСoordinates = function (ball, basket) {
+    let newCoordinates = new Coordinates(ball.box.center.x, ball.box.center.y);
+    let halfWidth = Math.floor(ball.box.width / 2);
+    let halfHeight = Math.floor(ball.box.height / 2);
+    if (isBasketEdge) {
+      isBasketEdge = false;
+      newCoordinates.x = basket.box.x0 - halfWidth - 1;
     }
-    
-
+    if (isFloor) {
+      isFloor = false;
+      newCoordinates.y = this.box.y0 + halfHeight + 1;
+    }
+    if (isCeiling) {
+      isCeiling = false;
+      newCoordinates.y = this.box.y1 - halfHeight - 1;
+    }
+    if (isLeftWall) {
+      isLeftWall = false;
+      newCoordinates.x = this.box.x0 + halfWidth + 1;
+    }
+    if (isRightWall) {
+      isRightWall = false;
+      newCoordinates.x = this.box.x1 - halfWidth - 1;
+    }
+    return newCoordinates;
+  };
 }
 
-function endGame(){
-    isEnd = true;
-    document.querySelector(`#new-game-button`).classList.toggle(`hide`);
-    let resultText = `Попробуй еще!!!`;
-    if(isWin) resultText = `ПОЗДРАВЛЯЮ`;
-    let resultElement = createElem(`div`,`result`,`body`,resultText);
+function endGame() {
+  isEnd = true;
+  let resultElem = document.querySelector(`.result`);
+  let resultText = `Попробуй еще!!!`;
+  if (isWin) resultText = `ПОЗДРАВЛЯЮ`;
+  resultElem.textContent = resultText;
+  resultElem.classList.remove(`hide`);
 }
 
-function shiftObject(obj){
-    obj.element.style.left = obj.box.x0+`px`;
-    obj.element.style.bottom = obj.box.y0+`px`;
-};
+function startNewGame() {
+  isWin = false;
+  isEnd = false;
+  document.querySelector(`#new-game-button`).classList.toggle(`hide`);
+}
+
+function shiftObject(obj) {
+  obj.element.style.left = obj.box.x0 + `px`;
+  obj.element.style.bottom = obj.box.y0 + `px`;
+}
--- src/interface.js
+++ src/interface.js
@@ -2,119 +2,169 @@ createInterface();
 
 const windowHeight = document.documentElement.clientHeight;
 const windowWidth = document.documentElement.clientWidth;
+const scale = 0.008;
+const startPosition = [100, 300];
 
-function createInterface(){
-    let room = createElem(`div`, `room`,`body`);
-    let ball = createElem(`div`, `ball`,`#room`);
-    dragElement(ball);
-    let basket = createElem(`div`,`basket`,`#room`);
-    let basketImg = createElem(`img`,`basket-img`,`#room`);
-    basketImg.setAttribute(`src`,`img/basket.png`);
-    let man = createElem(`div`,`man`,`#room`);
-    let manBody = createElem(`img`,`body`,`#man`);
-    manBody.setAttribute(`src`,`img/man.png`)
-    let manLeftArm = createElem(`div`,`left-arm`,`#man`);
-    let manRightArm = createElem(`div`,`right-arm`,`#man`);
+function createInterface() {
+  let room = createElem(`div`, `room`, `body`);
+  let dropZone = createElem(`div`, `drop-zone`, `#room`);
+  let ball = createElem(`div`, `ball`, `#drop-zone`);
+  let basket = createElem(`div`, `basket`, `#room`);
+  let basketImg = createElem(`img`, `basket-img`, `#room`);
+  basketImg.setAttribute(`src`, `img/basket.png`);
+  let man = createElem(`div`, `man`, `#drop-zone`);
+  let manBody = createElem(`img`, `body`, `#man`);
+  manBody.setAttribute(`src`, `img/man.png`);
+  let manLeftArm = createElem(`div`, `left-arm`, `#man`);
+  let manRightArm = createElem(`div`, `right-arm`, `#man`);
+  let result = createElem(`div`, ``, `body`);
+  result.classList.add(`hide`);
+  result.classList.add(`result`);
+  let info = createElem(`div`, ``, `body`);
+  info.classList.add(`info`);
+  info.classList.toggle(`hide`);
 
-    let newGameButton = createElem(`button`,`new-game-button`,`body`,`Начать игру!`);
+  let newGameButton = createElem(
+    `button`,
+    `new-game-button`,
+    `body`,
+    `Начать игру!`
+  );
+  newGameButton.addEventListener(`click`, (e) => {
+    result.classList.add(`hide`);
+    ball.style.left = startPosition[0] + `px`;
+    ball.style.bottom = startPosition[1] + `px`;
+    dragElement(ball, dropZone);
+    info.textContent = `Возьми мяч и брось в корзину!`;
+    info.classList.toggle(`hide`);
+    newGameButton.classList.toggle(`hide`);
+  });
 }
 
 function createElem(elem, id, parentSelector, text) {
-    let item = document.createElement(elem);
-    if (id) item.setAttribute(`id`, id);
-    item.textContent = text;
-    document.querySelector(parentSelector).appendChild(item);
+  let item = document.createElement(elem);
+  if (id) item.setAttribute(`id`, id);
+  item.textContent = text;
+  document.querySelector(parentSelector).appendChild(item);
 
-    return item;
+  return item;
 }
 
-function dragElement(elem){
-    let xMax = 600;
-    let yMax = 600;
-    let startX = 0;
-    let startY = 0;
-    let x0 =0;
-    let y0 =0;
-    let x1 = 0;
-    let y1=0;
-    let timer =0;
-    let dt = 0.05;
-    let speeds =[];
-    let angle = 0;
+function dragElement(elem, dropZone) {
+  let startX = 0;
+  let startY = 0;
+  let x0 = 0;
+  let y0 = 0;
+  let x1 = 0;
+  let y1 = 0;
+  let dx = 0;
+  let dy = 0;
+  let timer = 0;
+  let dt = 0.05;
+  let speeds = [];
+  let angle = 0;
 
-    elem.onmousedown = dragMouseDown;
+  elem.onmousedown = dragMouseDown;
 
+  function dragMouseDown(e) {
+    dropZone.classList.add(`select`);
+    document.querySelector(`.info`).classList.toggle(`hide`);
+    e = e || window.event;
+    e.preventDefault();
+    x1 = e.clientX;
+    y1 = e.clientY;
+    startX = x1;
+    startY = y1;
+    calcMouseShift();
+    timer = setInterval(trackSpeedAndAngle, dt * 1000);
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
 
-    function dragMouseDown(e){
-        e = e || window.event;
-        e.preventDefault();
-        x1 = e.clientX;
-        y1 = e.clientY;
-        startX = x1;
-        startY = y1;
-        timer = setInterval(trackSpeed,dt*1000);
-        document.onmouseup = closeDragElement;
-        document.onmousemove = elementDrag;
-
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    x0 = x1 - e.clientX;
+    y0 = y1 - e.clientY;
+    x1 = e.clientX;
+    y1 = e.clientY;
+    if (isOutOfDropZone()) {
+      closeDragElement();
+      return;
     }
+    elem.style.top = elem.offsetTop - y0 + `px`;
+    elem.style.left = elem.offsetLeft - x0 + `px`;
+  }
 
-    function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
-        x0 = x1 - e.clientX;
-        y0 = y1 - e.clientY;
-        x1 = e.clientX;
-        y1 = e.clientY;
-        if (x1 >550 || windowHeight - y1 > 550){
-            closeDragElement();
-            return;
-        }
-        elem.style.top = (elem.offsetTop - y0) + `px`;
-        elem.style.left = (elem.offsetLeft - x0) + `px`;
-      }
-    
-      function closeDragElement() {
-        document.onmouseup = null;
-        document.onmousemove = null;
-        elem.style.top = null;
-        elem.style.bottom = y1 + `px`;
-        clearInterval(timer);
-        let speed = 0;
-        if (speeds.length === 0) speed = 0;
-        else if (speeds.length < 5) {
-            speed = (speeds.reduce(
-                (previousValue, currentValue) => previousValue + currentValue))/speeds.length;
-        } else {
-            for(let i = 0; i<5;i++){
-                speed += speeds[speeds.length-1-i];
-            }
-            speed = speed/5;
-        }
-        console.log(angle,speed);
-        startGame(angle, speed*2);
-        speeds.length = 0;
-      }
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+    elem.onmousedown = null;
+    dropZone.classList.remove(`select`);
+    elem.style.top = null;
+    elem.style.bottom = windowHeight - y1 + `px`;
+    clearInterval(timer);
+    let speed = calcSpeed();
+    startGame(angle, speed * 1.5);
+  }
 
-      function trackSpeed(){
-          let x = x1 - startX;
-          let y = y1 - startY;
-          startX = x1;
-          startY = y1;
-          if (x === 0 && y > 0) angle = -Math.PI;
-          else if (x=== 0 && y < 0) angle = Math.PI;
-          else if (x===0 && y === 0) angle = 0;
-          else if (x > 0 && y > 0) angle = - Math.atan(y / x);
-          else if (x > 0) angle =  -Math.atan(y / x);
-          else if (y>0) angle = Math.PI - Math.atan(y / x);
-          else angle = Math.PI/2 + Math.atan(y / x);
-          let s = Math.pow(Math.pow(x,2) + Math.pow(y,2),1/2) * 0.008;
-          speeds.push(s/dt);
-      }
-  
-} 
+  function trackSpeedAndAngle() {
+    let x = x1 - startX;
+    let y = y1 - startY;
+    startX = x1;
+    startY = y1;
+    let prevAngle = angle;
+    calcAngle(x, y);
+    if (Math.abs(prevAngle - angle) >= Math.PI / 2) {
+      speeds.length = 0;
+    }
+    let s = Math.pow(Math.pow(x, 2) + Math.pow(y, 2), 1 / 2) * scale;
+    speeds.push(s / dt);
+  }
 
-    function setStartingValues(angle,speed){
-        startSpeed = speed;
-        startAngle = angle;
+  function isOutOfDropZone() {
+    let r = parseInt(window.getComputedStyle(dropZone).width, 10) / 2;
+    let centerX = parseInt(window.getComputedStyle(dropZone).left, 10) + r;
+    let centerY = parseInt(window.getComputedStyle(dropZone).bottom, 10) + r;
+    let ballX = x1 - dx;
+    let ballY = windowHeight - y1 - dy;
+    return (
+      Math.floor(Math.pow(ballX - centerX, 2) + Math.pow(ballY - centerY, 2)) >=
+      Math.floor(Math.pow(r, 2))
+    );
+  }
+
+  function calcMouseShift() {
+    let ball = document.querySelector(`#ball`);
+    let r = parseInt(window.getComputedStyle(ball).width, 10) / 2;
+    let x = parseInt(window.getComputedStyle(ball).left, 10) + r;
+    let y = parseInt(window.getComputedStyle(ball).bottom, 10) + r;
+    dx = x1 - x;
+    dy = windowHeight - y1 - y;
+  }
+
+  function calcAngle(x, y) {
+    if (x === 0 && y > 0) angle = -Math.PI;
+    else if (x === 0 && y < 0) angle = Math.PI;
+    else if (x === 0 && y === 0) angle = 0;
+    else if (x > 0 && y > 0) angle = 2 * Math.PI - Math.abs(Math.atan(y / x));
+    else if (x > 0) angle = -Math.atan(y / x);
+    else if (y > 0) angle = Math.PI - Math.atan(y / x);
+    else angle = Math.PI / 2 + Math.atan(y / x);
+  }
+
+  function calcSpeed() {
+    let speed = 0;
+    let speedsWithoutZeros = speeds.filter((value) => {
+      return value !== 0;
+    });
+    if (speedsWithoutZeros.length === 0) speed = 0;
+    else
+      speed =
+        speedsWithoutZeros.reduce(
+          (previousValue, currentValue) => previousValue + currentValue
+        ) / speedsWithoutZeros.length;
+    speeds.length = 0;
+    return speed;
+  }
 }
- 
